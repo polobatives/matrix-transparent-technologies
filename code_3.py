5b5377538ca7d349a81376b42f741efd4e41ef49
polobatives <143891829+polobatives@users.noreply.github.com>
2023-10-20 16:46:16 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+import random
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    main()
+def generate_random_data():
+
+    for item in data:
+if __name__ == "__main__":
+    data = generate_random_data()
+
+
+        print(f"Random Number: {item}")        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+
+}
+    });
+        console.log("Random Number: " + item);
+    return data;
+function generateRandomData() {
+
+main();
+    const data = [];
+
+    const data = generateRandomData();
+    data.forEach(item => {
+}
+    }
+function main() {
