cf5dfd2677d676cf2b4c38d79718a5d41fcb0859
polobatives <143891829+polobatives@users.noreply.github.com>
2023-11-01 04:18:28 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,63 +1,30 @@
-if __name__ == "__main__":
-
-
-    for item in data:
-import random
-    data = generate_random_data()
-def generate_random_data():
-
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
     main()
-def main():
-
-    return data    main()
-import random
 if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
 def main():
-
     data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-
 
     return data
-        print(f"Random Number: {item}")
-
-def generate_random_data():
-    data = generate_random_data()
-    main()
-import random
-if __name__ == "__main__":
-
     for item in data:
-
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
 def generate_random_data():
-
-    return data
-def main():            data[i] = (int) (Math.random() * 100) + 1;
-        }
-    }
-        for (int item : data) {
-        }
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-
+import random
+function main() {
 }
-
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-    public static void main(String[] args) {        }
-        }
-    }
-        for (int i = 0; i < 10; i++) {
+    return data;
+    const data = generateRandomData();
 }
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-public class RandomDataGenerator {
-            data[i] = (int) (Math.random() * 100) + 1;
+        console.log("Random Number: " + item);
 
-        for (int item : data) {
-    public static void main(String[] args) {
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+
+    data.forEach(item => {
+    });
+    }
